0114ec349020156e7069825077808fb65363b970
accopeland <accopeland@lbl.gov>
2023-04-14 11:55:09 -0700

    PKG:perl-mime-base64 ; WIP:minikube, buildah ; BUG: perl-net-ssleay

--- var/spack/repos/builtin/packages/buildah/package.py
+++ var/spack/repos/builtin/packages/buildah/package.py
@@ -7,7 +7,33 @@ from spack.package import *
 
 
 class Buildah(Package):
-    """A tool that facilitates building OCI images."""
+    """A tool that facilitates building OCI images
+
+    FIXME: work in progress
+
+    => buildah: Executing phase: 'install'
+    ==> Error: ProcessError: Command exited with status 2:
+    'make' '-j16'
+
+4 errors found in build log:
+     32    # pkg-config --cflags  -- devmapper
+     33    Package devmapper was not found in the pkg-config search path.
+     34    Perhaps you should add the directory containing `devmapper.pc'
+     35    to the PKG_CONFIG_PATH environment variable
+     36    No package 'devmapper' found
+     37    pkg-config: exit status 1
+  >> 38    make: *** [Makefile:92: bin/imgtype] Error 2
+     39    make: *** Waiting for unfinished jobs....
+  >> 40    make: *** [Makefile:98: bin/tutorial] Error 2
+  >> 41    make: *** [Makefile:73: bin/buildah] Error 2
+  >> 42    make: *** [Makefile:95: bin/copy] Error 2
+     43    make[1]: Leaving directory '/tmp/accopeland/spack-stage/spack-stage-buildah-1.29.1-sjhepbljoxvr4uy3yhoixewocjwif6sm/spack-src
+           /tests/tools'
+
+See build log for details:
+  /tmp/accopeland/spack-stage/spack-stage-buildah-1.29.1-sjhepbljoxvr4uy3yhoixewocjwif6sm/spack-build-out.txt
+
+    ."""
 
     homepage = "https://github.com/containers/buildah"
     url = "https://github.com/containers/buildah/archive/refs/tags/v1.29.1.tar.gz"
--- var/spack/repos/builtin/packages/perl-net-ssleay/package.py
+++ var/spack/repos/builtin/packages/perl-net-ssleay/package.py
@@ -12,12 +12,22 @@ class PerlNetSsleay(PerlPackage):
     """Perl extension for using OpenSSL"""
 
     homepage = "https://metacpan.org/pod/Net::SSLeay"
-    url = "http://search.cpan.org/CPAN/authors/id/M/MI/MIKEM/Net-SSLeay-1.82.tar.gz"
+    #url = "http://search.cpan.org/CPAN/authors/id/M/MI/MIKEM/Net-SSLeay-1.82.tar.gz"
+    #url = "https://cpan.metacpan.org/authors/id/C/CH/CHRISN/Net-SSLeay-1.92.tar.gz"
+    url = "https://cpan.metacpan.org/authors/id/C/CH/CHRISN/Net-SSLeay-1.93_02.tar.gz"
 
+    version("1.93_02", sha256="1a11d1ae63e9fc85c90279085957aec81a15af985d7cff185b66154f7032fcdf")
+    version("1.93_01", sha256="876d022fbc719631b11d6bb4b6e78db3c19bbca578093c376c8f9900a4432aa3")
+    version("1.92", sha256="47c2f2b300f2e7162d71d699f633dd6a35b0625a00cbda8c50ac01144a9396a9")
+    version("1.91_03", sha256="a8a5c30ae78a932ee3d19764a07285c37711f4e76d4004be8d1093eb193aaa39")
+    version("1.91_02", sha256="770bf7c420b7083d3c9c1f46fd3b1497f20c38623ee0f6eb792331b0d79f9748")
+    version("1.91_01", sha256="bae9400f0655dfb39b0779d3f6445a52954705ad75cccdd7d6e55a6f13b65dfa")
+    version("1.90", sha256="f8696cfaca98234679efeedc288a9398fcf77176f1f515dbc589ada7c650dc93")
     version("1.85", sha256="9d8188b9fb1cae3bd791979c20554925d5e94a138d00414f1a6814549927b0c8")
     version("1.82", sha256="5895c519c9986a5e5af88e3b8884bbdc70e709ee829dc6abb9f53155c347c7e5")
 
     depends_on("openssl")
+    depends_on("perl-mime-base64")
 
     def configure(self, spec, prefix):
         self.build_method = "Makefile.PL"
