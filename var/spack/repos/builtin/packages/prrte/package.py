e6523e7fed98cd1e6009402f11c192d78a2dc5dd
accopeland <accopeland@lbl.gov>
2024-05-28 21:13:14 -0700

    new package: prrte

--- var/spack/repos/builtin/packages/prrte/package.py
+++ var/spack/repos/builtin/packages/prrte/package.py
@@ -24,6 +24,12 @@ class Prrte(AutotoolsPackage):
     license("BSD-3-Clause-Open-MPI")
 
     version("develop", branch="master")
+    version("3.0.5", sha256="75ce732b02f3bc7eff5e51b81469e4373f1effc6a42d8445e2935d3670e58c8e")
+    version("3.0.4", sha256="7394c2ef9ea548cbc223b62a943f470cfbccf74b3879ac92564d148537f229df")
+    version("3.0.3", sha256="25b30a6252ecaeb98d3c2244710493ee5d0bbc31bfa939a42df391bde7293b80")
+    version("3.0.2", sha256="1aaa1bb930e8e940251ea682b4a6abc24e4849fa9ffbaaaaf2750a38ba4e474a")
+    version("3.0.1", sha256="98fe184b191e78571877492620cc90dd5d46b603a64490fa8356843b39628683")
+    version("3.0.0", sha256="0898797e5530e2e37f248a1b32d572828cb3304b0c427b376ea006a1452ba565")
     version("1.0.0", sha256="a9b3715e059c10ed091bd6e3a0d8896f7752e43ee731abcc95fb962e67132a2d")
 
     depends_on("pmix")
